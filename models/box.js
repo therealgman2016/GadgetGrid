03d7927dcaa83e54d8141afd05912e8027485da6
Gabriel Moore <125038221+therealgman2016@users.noreply.github.com>
2023-09-14 14:04:50 -0400

    commit

--- models/box.js
+++ models/box.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
+const itemSchema = require('./item')
 
 const boxSchema = new Schema({
     label: String,
-    itemProperites: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Item'
-        }
-    ]
+    itemProperties: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Item'
+    }]
 }, {
     timestamps: true
 })
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// BROKE
+
 require('dotenv').config();// loads env file
 require('./config/database'); // loads database connection
 
@@ -6,31 +8,29 @@ const Item = require('./models/item');
 
 async function createItemAndBox() {
   try {
-    // Create an Item document
-    const newItem = new Item({
-      name: 'Item 1',
-      type: 'Type 1',
-      dateAdded: new Date(),
-      location: 'Location 1',
-      // other item properties
-    });
-
-    // Save the Item document
-    const savedItem = await newItem.save();
+    
 
     // Create a Box document and copy relevant properties from the Item
     const newBox = new Box({
       name: 'LTS',
-      itemProperties: {
-        name: savedItem.name,
-        type: savedItem.type,
-        dateAdded: savedItem.dateAdded,
-        location: savedItem.location,
       },
-    });
+    );
 
     // Save the Box document
     await newBox.save(); // You can await here to ensure it's saved
+
+    // Create an Item document
+    const newItem = ({
+        name: 'Item 2',
+        type: 'Type 1',
+        dateAdded: new Date(),
+        location: 'Location 1',
+        // other item properties
+    })
+
+    newBox.itemProperties.push(newItem)
+
+    await newBox.save()
 
     console.log('Box saved with associated Item properties:', newBox);
   } catch (err) {
